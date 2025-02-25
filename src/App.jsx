d2cb3fdc824074b27a94ec78fccf879075a64c82
Garusinha <nikethasenarath@gmail.com>
2025-02-26 00:07:39 +0800

    Updated website content

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
+import Service from "./components/Service";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
+      <Service />
     </div>
   );
 };
